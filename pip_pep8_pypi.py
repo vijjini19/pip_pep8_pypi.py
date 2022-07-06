a0bf477819ee5f143d3a0f5991ed1b3f369e8bbb
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-06 21:44:35 +0530

    Add files via upload

---
+++ pip_pep8_pypi.py
@@ -0,0 +1,11 @@
+'''pip-python intsallation package
+pip is a package manager for python
+
+PEP8-python enhanced proposal
+coding style conversion
+
+pypi-python package index
+
+pypi.org-website for all packages available in python
+
+Batteries_included-https://www.pythonanyehere.com/batteries_included/ '''
